10ece344dd9518e4ea8d67ca44a89131fba823db
Mahesh SN <mahesh@educosoft.com>
2021-11-27 00:03:02 +0530

    Checkin after adding leaves tables

---
+++ Leave Management NET6/Data/LeaveAllocation.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Leave_Management_NET6.Data
+{
+    public class LeaveAllocation
+    {
+        public int NumberOfDays { get; set; }
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public string EmployeeId { get; set; }
+
+    }
+}
---
+++ Leave Management NET6/Data/LeaveType.cs
@@ -0,0 +1,8 @@
+﻿namespace Leave_Management_NET6.Data
+{
+    public class LeaveType
+    {
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+    }
+}
